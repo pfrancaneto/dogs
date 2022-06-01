5daff7603c8b98e42b73196cb9aa21d323f96a13
Pedro França <pfranca.neto@gmail.com>
2022-06-01 00:48:11 -0300

    ajustando componentes com css

--- src/Components/Forms/Button.js
+++ src/Components/Forms/Button.js
@@ -1,8 +1,12 @@
-import React from "react";
-import styles from "./Button.module.css";
+import React from 'react';
+import styles from './Button.module.css';
 
 const Button = ({ children, ...props }) => {
-  return <button {...props} className={styles.button}>{children}</button>;
+  return (
+    <button {...props} className={styles.button}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
